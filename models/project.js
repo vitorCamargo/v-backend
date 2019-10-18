dfdc9acce3587570a98c396d79c75e1ae9d83c31
Vitor Bueno <vitorcamargo@alunos.utfpr.edu.br>
2019-10-18 11:30:52 -0300

    Initial Structure

---
+++ models/project.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const uniquevalidator = require('mongoose-unique-validator');
+
+const { Schema } = mongoose;
+
+const ProjectSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  colorPrimary: { type: String, required: true },
+  colorSecondary: { type: String },
+  images: {
+    desktop: { type: String },
+    mobile: { type: String }
+  },
+  links: {
+    github: { type: String },
+    url: { type: String },
+    googlePlay: { type: String },
+    others: [{
+      image: { type: String },
+      url: { type: String }
+    }]
+  }
+}, { timestamps: true });
+
+ProjectSchema.plugin(uniquevalidator);
+
+const Project = mongoose.model('Project', ProjectSchema, 'Projects');
+
+module.exports = Project;
+
+module.exports.getAllProjects = (callback) => {
+  Project.find(callback);
+};
+
+module.exports.getProjectById = (id, callback) => {
+  Project.findOne({ _id: id }, callback);
+};
+
+module.exports.addProject = (project, callback) => {
+  Project.create(project, callback);
+};
+
+module.exports.updateProject = (id, updatedProject, callback) => {
+  Project.getProjectById(id, (err, project) => {
+    if (err) callback(err, null);
+
+    project.name = updatedProject.name ? updatedProject.name : project.name;
+    project.description = updatedProject.description ? updatedProject.description : project.description;
+    project.colorPrimary = updatedProject.colorPrimary ? updatedProject.colorPrimary : project.colorPrimary;
+    project.colorSecondary = updatedProject.colorSecondary ? updatedProject.colorSecondary : project.colorSecondary;
+    project.images = updatedProject.images ? updatedProject.images : project.images;
+    project.links = updatedProject.links ? updatedProject.links : project.links;
+
+    project.save(callback);
+  });
+};
+
+module.exports.deleteProject = (id, callback) => {
+  Project.deleteOne({ _id: id }, callback);
+};
---
+++ routes/project.js
@@ -0,0 +1,72 @@
+const express = require('express');
+
+const Project = require('../models/project');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  Project.getAllProjects((err, projects) => {
+    if(err) {
+      console.log(err);
+      res.status(400).send('Can\'t find all projects \n');
+    }
+    res.status(200).json(projects);
+  });
+});
+
+router.post('/', (req, res) => {
+  const {
+    name, description, colorPrimary, colorSecondary, images, links
+  } = req.body;
+
+  const newProject = {};
+  newProject.name = name;
+  newProject.description = description;
+  newProject.colorPrimary = colorPrimary;
+  newProject.colorSecondary = colorSecondary;
+  newProject.images = images;
+  newProject.links = links;
+
+  Project.addProject(newProject, (err, project) => {
+    if(err) {
+      console.log(err);
+      res.status(400).send('Can\'t create the project \n');
+    }
+    res.status(200).json(project);
+  });
+});
+
+router.put('/', (req, res) => {
+  const {
+    id, name, description, colorPrimary, colorSecondary, images, links
+  } = req.body;
+
+  const updatedProject = {};
+  
+  updatedProject.name = name;
+  updatedProject.description = description;
+  updatedProject.colorPrimary = colorPrimary;
+  updatedProject.colorSecondary = colorSecondary;
+  updatedProject.images = images;
+  updatedProject.links = links;
+
+  Project.updateProject(id, updatedProject, (err, project) => {
+    if(err) {
+      console.log(err);
+      res.status(400).send('Can\'t update this Project \n');
+    }
+    res.status(200).json(project);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  Project.deleteProject(req.params.id, (err, project) => {
+    if(err) {
+      console.log(err);
+      res.status(400).send('Can\'t delete this project \n');
+    }
+    res.status(200).json(project);
+  });
+});
+
+module.exports = router;
